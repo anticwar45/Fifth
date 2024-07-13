3968378537377f5107297b152a24ac1a7543ed1d
anticwar45 <anticwar4@rambler.ru>
2024-07-13 19:30:41 +0700

    update BaseTest.java

--- src/test/java/mantis/tests/BaseTest.java
+++ src/test/java/mantis/tests/BaseTest.java
@@ -1,24 +1,19 @@
 package mantis.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import mantis.pages.MantisSite;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class BaseTest {
     public WebDriver driver;
-    String browser = System.getProperty("browser");
 
     @BeforeEach
     public void setUp() {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
 
         driver.get("https://academ-it.ru/mantisbt/login_page.php");
         driver.manage().window().maximize();
